2a50e81bed5bdf1083dc919be4435bffd92edfeb
maxnatalia <Nataliamazur1988@gmail.com>
2022-11-10 09:39:12 +0100

    added new component styled Button

---
+++ src/common/Button/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    background-color: #dc952a;
+    color: white;
+    font-weight: 700;
+    border-radius: 20px;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+
+    &:hover {
+        filter: brightness(120%);
+    }
+`;
