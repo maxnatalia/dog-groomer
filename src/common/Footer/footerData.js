051c7d0177bf4266942da829e1473a702e5b197a
maxnatalia <Nataliamazur1988@gmail.com>
2022-11-10 08:04:19 +0100

    added footerData.js

---
+++ src/common/Footer/footerData.js
@@ -0,0 +1,16 @@
+import { AiFillYoutube, AiFillInstagram, AiFillGithub } from 'react-icons/ai';
+
+export const social = [
+    {
+      icon: <AiFillYoutube />,
+      href: '#',
+    },
+    {
+      icon: <AiFillInstagram />,
+      href: '#',
+    },
+    {
+      icon: <AiFillGithub />,
+      href: '#',
+    },
+  ];
