ef492c563ac763dce063478b72c15c147c80ea3b
maxnatalia <Nataliamazur1988@gmail.com>
2022-11-11 07:32:41 +0100

    added main Route in App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomePage from "./features/HomePage";
+import PricingPage from "./features/PricingPage";
+import AppointmentPage from "./features/AppointmentPage";
+import Navigation from "./common/Navigation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Navigation />
+      <Switch>
+        <Route path="/price"><PricingPage /></Route>
+        <Route path="/appointment"><AppointmentPage /></Route>
+        <Route path="/"><HomePage /></Route>
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
