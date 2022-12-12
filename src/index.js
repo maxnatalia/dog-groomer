293a45bc5507e808d57577c58910750671f276b7
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-12 14:17:24 +0100

    added normalize and globalstyles to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Normalize } from 'styled-normalize';
+import { GlobalStyle } from "./GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Normalize />
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
