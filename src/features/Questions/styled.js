4e36ff2d3243dc58f3bf346399e7ac16f391625c
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-30 13:53:39 +0100

    added basic styles to Questions

---
+++ src/features/Questions/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+    margin-top: 100px;
+    height: 100vh;
+`;
+
+export const Section = styled.section`
+    max-width: 1170px;
+    margin: 0 auto;
+    padding: 50px;
+`;
