099d6823f14e959b0e23e8bf593e0d37200ffd31
maxnatalia <Nataliamazur1988@gmail.com>
2022-12-31 14:25:07 +0100

    deleted unnecessary code

--- src/features/Questions/index.js
+++ src/features/Questions/index.js
@@ -1,16 +1,13 @@
-import { useState } from "react";
 import data from "./data";
 import { Main, Section } from "./styled";
 import Question from "./Question";
 
-
 const Questions = () => {
-  const [questions, setQuestions] = useState(data);
 
   return (
     <Main>
       <Section>
-        {questions.map((question) =>
+        {data.map((question) =>
           <Question key={question.id} {...question} />
         )}
       </Section>
