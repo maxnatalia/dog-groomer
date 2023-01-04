2dd3eaf6be9dc342309c704c36d9ec4bcd18b5bd
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-04 09:22:09 +0100

    created new route for Slider

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AppointmentPage from "./features/AppointmentPage";
 import Navigation from "./common/Navigation";
 import Reminder from "./features/Reminder";
 import Questions from "./features/Questions";
+import Slider from "./features/Slider";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/appointment"><AppointmentPage /></Route>
         <Route path="/reminder"><Reminder /></Route>
         <Route path="/questions"><Questions /></Route>
+        <Route path="/slider"><Slider /></Route>
         <Route path="/"><HomePage /></Route>
       </Switch>
     </BrowserRouter>
