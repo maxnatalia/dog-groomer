81b0eb9cc1ed3e88ee9b4ef82f113df5621c5e66
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-18 15:02:48 +0100

    added styles to Error Page

---
+++ src/features/ErrorPage/styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: beige;
+`;
+
+export const Title = styled.h1`
+    font-size: 100px;
+    color: orange;
+`;
+
+export const Info = styled.p`
+    font-size: 40px;
+`;
