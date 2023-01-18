91df9b7b2b493dd2114d4642557088b8d42340dd
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-18 07:27:21 +0100

    added necessary styles to Content

--- src/features/HomePage/Content/styled.js
+++ src/features/HomePage/Content/styled.js
@@ -1,10 +1,24 @@
 import styled from "styled-components";
 
 export const Section = styled.section`
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const Header = styled.h2`
+    font-size: 46px;
+    font-weight: 600;
+    text-align: center;
 `;
 
 export const Container = styled.div`
+    max-width: 900px;
     display: flex;
+    justify-items: center;
+    gap: 40px;
+    
 `;
 
 export const ImageWrapper = styled.div`
@@ -12,6 +26,7 @@ export const ImageWrapper = styled.div`
 
 export const Img = styled.img`
     margin: 20px;
+    width: 20vw;
 `;
 
 export const ContentWrapper = styled.div`
