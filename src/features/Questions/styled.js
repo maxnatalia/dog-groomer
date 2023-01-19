c9f446db3cbc6fc423e3e2bb13b84a6cf0e44751
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-19 12:43:23 +0100

    improved styles in Questions component

--- src/features/Questions/styled.js
+++ src/features/Questions/styled.js
@@ -1,12 +1,37 @@
 import styled from "styled-components";
 
-export const Main = styled.main`
-    margin-top: 100px;
-    height: 100vh;
-`;
-
 export const Section = styled.section`
-    max-width: 1170px;
+    max-width: 900px;
     margin: 0 auto;
     padding: 50px;
+    text-align: center;
+`;
+
+export const Container = styled.div`
+    margin-bottom: 20px;
+`;
+
+export const Paragraph = styled.p`
+    font-size: 22px;
+
+    @media (max-width: 767px) {
+        font-size: 18px;
+    }
+`;
+
+export const Button = styled.button`
+    background-color: #dc952a;
+    color: white;
+    border-radius: 50%;
+    padding: 10px;
+    margin: 20px auto;
+    cursor: pointer;
+    border: 2px solid orange;
+    transition: 1s ease-in-out;
+
+    &:hover {
+        border:2px solid orange;
+        background-color: transparent;
+        color: orange;
+    }
 `;
