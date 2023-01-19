fa2e823db81a68d1f8905c9ba4f33c7ebcc6f004
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-19 10:08:20 +0100

    added basic styles for PracingPage

---
+++ src/features/PricingPage/styled.js
@@ -0,0 +1,52 @@
+import styled, { css } from "styled-components";
+
+export const Section = styled.section`
+    margin: 100px auto;
+    text-align: center;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 20px;
+    padding: 20px;
+`;
+
+export const Category = styled.div`
+    padding: 28px;
+    cursor: pointer;
+    transition: 1s ease-in-out;
+    border-bottom: 2px solid orange;
+
+    &:hover {
+        transform: scale(1.1);
+        border-bottom: 6px solid orange;
+    }
+
+    ${({ activeDog }) => activeDog && css`
+        transform: scale(1.1);
+        border-bottom: 6px solid orange;
+    `}
+`;
+
+export const PriceContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+export const CategoryName = styled.h3`
+    font-size: 30px;
+
+    ${({ activeDog }) => activeDog && css`
+        text-shadow: 4px 4px 6px orange;
+    `}
+`;
+
+export const CategoryWeight = styled.p`
+    font-size: 20px;
+
+    ${({ activeDog }) => activeDog && css`
+        text-shadow: 4px 4px 6px orange;
+    `}
+`;
