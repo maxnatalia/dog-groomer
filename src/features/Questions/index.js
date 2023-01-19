ccd51744536a2e329ef47071e2396319c6d0bb0c
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-19 12:42:57 +0100

    added new element to Questions component

--- src/features/Questions/index.js
+++ src/features/Questions/index.js
@@ -1,17 +1,27 @@
 import data from "./data";
-import { Main, Section } from "./styled";
+import { Section, Container, Paragraph } from "./styled";
 import Question from "./Question";
+import { Header } from "../../common/Header/styled";
+import { Button } from "../../common/Button/styled";
+import { Title } from "../../common/Title/styled";
 
 const Questions = () => {
 
   return (
-    <Main>
-      <Section>
+    <Section>
+      <Header>Q&A</Header>
+      <Container>
         {data.map((question) =>
           <Question key={question.id} {...question} />
         )}
-      </Section>
-    </Main>
+      </Container>
+      <div>
+        <Header>Want To Know More?</Header>
+        <Title>Have a question that isn't answered here?</Title>
+        <Paragraph>Get in touch and we will be happy to help.🐶</Paragraph>
+        <Button to="/appointment">Contact Us</Button>
+      </div>
+    </Section>
   )
 };
 
