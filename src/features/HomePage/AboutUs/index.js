02ce62e8748faac3854e9165c21515c48cbcc2cb
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-19 12:42:14 +0100

    added new component AboutUs

---
+++ src/features/HomePage/AboutUs/index.js
@@ -0,0 +1,21 @@
+import { Header } from "../../../common/Header/styled";
+
+const AboutUs = () => {
+    return (
+        <section>
+            <Header>About Us</Header>
+            <div>
+                <h3>Why Choose Us?</h3>
+                <ul>
+                    <li>Experienced, Professional Dog Groomer</li>
+                    <li>Grooming dogs since 2010</li>
+                    <li>One-to-one attention for your dog</li>
+                    <li>Homely atmosphere</li>
+                    <li>Dogs aren’t put in cages or tied up</li>
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default AboutUs;
