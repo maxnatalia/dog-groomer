80fb72b91827b0a87c0e319307c309b4e2ab8a06
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-19 10:07:43 +0100

    added new date and images for PricingPage

---
+++ src/features/PricingPage/dataServicePrice.js
@@ -0,0 +1,155 @@
+import dogCateg1 from '../PricingPage/dog-categ-1.png';
+import dogCateg2 from '../PricingPage/dog-categ-2.png';
+import dogCateg3 from '../PricingPage/dog-categ-3.png';
+import dogCateg4 from '../PricingPage/dog-categ-4.png';
+
+export const dataServicePrice = [
+    {
+        id: 1,
+        image: dogCateg1,
+        name: 'small',
+        dogCategory: '1 - 9 kg',
+        services: [
+            {
+                name: 'smart',
+                price: 20,
+                list: ['wash', 'ears cleaning', 'nail cut', 'perfumed'],
+            },
+            {
+                name: 'premium',
+                price: 40,
+                list: [
+                    'all smart services',
+                    'service 1',
+                    'service 2',
+                    'service 3',
+                    'service 4',
+                ],
+            },
+            {
+                name: 'royal',
+                price: 60,
+                list: [
+                    'all premium services',
+                    'service 1',
+                    'service 2',
+                    'service 3',
+                    'service 4',
+                    'service 5',
+                    'service 6',
+                ],
+            },
+        ],
+    },
+    {
+        id: 2,
+        name: 'medium',
+        image: dogCateg2,
+        dogCategory: '10 - 19 kg',
+        services: [
+            {
+                name: 'smart',
+                price: 80,
+                list: ['wash', 'ears cleaning', 'nail cut', 'perfumed'],
+            },
+            {
+                name: 'premium',
+                price: 100,
+                list: [
+                    'all smart services',
+                    'service 1',
+                    'service 2',
+                    'service 3',
+                    'service 4',
+                ],
+            },
+            {
+                name: 'royal',
+                price: 120,
+                list: [
+                    'all premium services',
+                    'service 1',
+                    'service 2',
+                    'service 3',
+                    'service 4',
+                    'service 5',
+                    'service 6',
+                ],
+            },
+        ],
+    },
+    {
+        id: 3,
+        name: 'big',
+        image: dogCateg3,
+        dogCategory: '20 - 29 kg',
+        services: [
+            {
+                name: 'smart',
+                price: 140,
+                list: ['wash', 'ears cleaning', 'nail cut', 'perfumed'],
+            },
+            {
+                name: 'premium',
+                price: 160,
+                list: [
+                    'all smart services',
+                    'service 1',
+                    'service 2',
+                    'service 3',
+                    'service 4',
+                ],
+            },
+            {
+                name: 'royal',
+                price: 180,
+                list: [
+                    'all premium services',
+                    'service 1',
+                    'service 2',
+                    'service 3',
+                    'service 4',
+                    'service 5',
+                    'service 6',
+                ],
+            },
+        ],
+    },
+    {
+        id: 4,
+        name: 'super',
+        image: dogCateg4,
+        dogCategory: '30 - 39 kg',
+        services: [
+            {
+                name: 'smart',
+                price: 200,
+                list: ['wash', 'ears cleaning', 'nail cut', 'perfumed'],
+            },
+            {
+                name: 'premium',
+                price: 210,
+                list: [
+                    'all smart services',
+                    'service 1',
+                    'service 2',
+                    'service 3',
+                    'service 4',
+                ],
+            },
+            {
+                name: 'royal',
+                price: 220,
+                list: [
+                    'all premium services',
+                    'service 1',
+                    'service 2',
+                    'service 3',
+                    'service 4',
+                    'service 5',
+                    'service 6',
+                ],
+            },
+        ],
+    },
+];
