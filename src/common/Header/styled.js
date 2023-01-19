04e7f2562685f3123670935b41330384584f492e
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-19 09:34:58 +0100

    created common global component Header

---
+++ src/common/Header/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Header = styled.h2`
+    font-size: 62px;
+    letter-spacing: 1.7px;
+    text-align: center;
+    color: orange;
+    text-shadow: 4px 4px 6px gray;
+
+    @media (max-width: 767px) {
+        font-size: 30px;
+    }
+`;
