f908e820c8e261ee0b8bb543ee9c56dbc599b26f
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-20 09:41:40 +0100

    updated questions data

--- src/features/Questions/data.js
+++ src/features/Questions/data.js
@@ -1,33 +1,39 @@
 const questions = [
-    {
-      id: 1,
-      title: 'Do I have to allow the use of cookies?',
-      info:
-        'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
-    },
-    {
-      id: 2,
-      title: 'How do I change my My Page password?',
-      info:
-        'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
-    },
-    {
-      id: 3,
-      title: 'What is BankID?',
-      info:
-        'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
-    },
-    {
-      id: 4,
-      title: 'Whose birth number can I use?',
-      info:
-        'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
-    },
-    {
-      id: 5,
-      title: 'When do I recieve a password ordered by letter?',
-      info:
-        'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
-    },
-  ]
-  export default questions;
+  {
+    id: 1,
+    title: 'How old should my puppy be before he gets his first groom?',
+    info:
+      'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+  },
+  {
+    id: 2,
+    title: 'What is your latest appointment of the day?',
+    info:
+      'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+  },
+  {
+    id: 3,
+    title: 'What is included in a bath?',
+    info:
+      'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+  },
+  {
+    id: 4,
+    title: 'How often should I get my dog groomed?',
+    info:
+      'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+  },
+  {
+    id: 5,
+    title: 'How often do nails need trimming?',
+    info:
+      'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+  },
+  {
+    id: 6,
+    title: 'How long does it usually take?',
+    info:
+      'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+  },
+]
+export default questions;
