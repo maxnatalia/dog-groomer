b6ccc4ed1228dd28ec9ff56ecfd155cd94849dc6
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-21 12:10:17 +0100

    added styles to Testimonials

---
+++ src/features/HomePage/Testimonials/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const Section = styled.section`
+    padding: 20px;
+`;
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 20px;
+    margin-top: 20px;
+`;
