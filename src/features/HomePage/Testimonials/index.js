023cc4d125bdeb38fb0e5a6c1e11de7a2a101ec8
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-21 12:10:05 +0100

    added Testimonials component

---
+++ src/features/HomePage/Testimonials/index.js
@@ -0,0 +1,25 @@
+import { Header } from "../../../common/Header/styled";
+import { Tile } from "../../../common/Tile/styled";
+import { Title } from "../../../common/Title/styled";
+import { Container, Section } from "./styled";
+import dataTestimonials from "./dataTestimonials";
+
+const Testimonials = () => {
+    return (
+        <Section>
+            <Header>Testimonials</Header>
+            <Title>See What Our Satisfied Customers Have To Say🐶</Title>
+            <Container>
+                {dataTestimonials.map((item) => {
+                    return (<Tile key={item.id}>
+                        <h3>{item.name}</h3>
+                        <p>{item.info}</p>
+                        <span>🐶🐶🐶🐶🐶</span>
+                    </Tile>)
+                })}
+            </Container>
+        </Section>
+    )
+}
+
+export default Testimonials;
