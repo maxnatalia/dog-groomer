9ad4dce3d33f0368d7b7a3a71b3d29987ab5c3e7
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-21 12:08:45 +0100

    added main components to HomePage

---
+++ src/features/HomePage/index.js
@@ -0,0 +1,17 @@
+import Carousel from "./Carousel";
+import Content from "./Content";
+import AboutUs from "./AboutUs";
+import Testimonials from "./Testimonials";
+
+const HomePage = () => {
+    return (
+        <>
+            <Carousel />
+            <Content />
+            <AboutUs />
+            <Testimonials />
+        </>
+    )
+};
+
+export default HomePage;
