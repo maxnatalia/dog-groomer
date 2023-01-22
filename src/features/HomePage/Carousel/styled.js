f0bb86b3da003be91a8b8d5ba432bec444016da0
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-22 08:51:28 +0100

    added new background for Carousel

--- src/features/HomePage/Carousel/styled.js
+++ src/features/HomePage/Carousel/styled.js
@@ -1,97 +1,54 @@
 import styled, { css } from "styled-components";
+import hero from "./bubbles.svg";
 
-export const Section = styled.section`
+export const SectionHero = styled.section`
     width: 100%;
-`;
-
-export const Button = styled.button`
-    border-radius: 50%;
-    position: absolute;
-    top: 200px;
-    transform: translateY(-50%);
-    background: grey;
-    color: white;
-    display: grid;
-    place-items: center;
-    border-color: transparent;
-    font-size: 1rem;
-    border-radius: 0.25rem;
-    padding: 15px;
-    z-index: -50;
-    cursor: pointer;
-    transition: all 0.3s linear;
-
-    &:hover {
-        background: lightgrey;
-    }
-
-    ${({ left }) => left && css`
-        left: 0;
-    `}
-
-    ${({ right }) => right && css`
-        right: 0;
-    `}
+    height: 100vh;;
+    background-image: url("${hero}");
+    background-color: beige;
+    padding: 30px;
 `;
 
 export const Container = styled.div`
-    background-color: beige;
-    z-index: -50;
+    margin-top: 80px;
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
     position: relative;
-    overflow: hidden;
-    padding: 100px;
-    margin: 0 auto;
-    
-    &::after {
-        content: "";
-        width: 650px;
-        height: 650px;
-        background-color: #fcdd0030;
-        position: absolute;
-        border-radius: 50%;
-        z-index: -20;
-        top: -20px;
-        right: -185px;
-    }
 `;
-
 
 export const Wrapper = styled.div`
-    padding: 50px;
     display: flex;
-    justify-content: space-between;
-`;
+    justify-content: start;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    transition: all 0.3s linear;
 
+    ${({ activeSlide }) => activeSlide && css`
+        opacity: 1;
+        transform: translateX(0);
+    `}
+
+    @media (max-width: 767px) {
+        flex-direction: column-reverse;
+        align-items: center;
+    }
+`;
 
 export const Img = styled.img`
-    z-index: 100;
-    height: auto;
-
-    @media (max-width: 768px) {
-        width: 100px;
-    }
-`;
-
-export const Title = styled.h1`
-    font-size: 42px;
-    color: blue;
-
-    @media (max-width: 768px) {
-        font-size: 20px;
-    }
-`;
-
-export const Subtitle = styled.h2`
-    @media (max-width: 768px) {
-        font-size: 12px;
-    }
+    max-width: 40vw;
+    margin-top: 20px;
+    object-fit: cover;
 `;
 
 export const ContentWrapper = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
-    align-items: flex-start;
-    padding: 50px;
-    max-width: 700px;
+    text-align: center;
 `;
