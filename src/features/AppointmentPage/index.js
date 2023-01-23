a9bb0c3c5a8b98e6b67ed2c747db8370cd4b1f0b
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-23 09:22:17 +0100

    added interactions for Appointment

--- src/features/AppointmentPage/index.js
+++ src/features/AppointmentPage/index.js
@@ -1,8 +1,92 @@
-
+import { Header } from "../../common/Header/styled";
+import { Tile } from "../../common/Tile/styled";
+import { Title } from "../../common/Title/styled";
+import { Paragraph } from "../../common/Paragraph/styled";
+import { Container, Section, Fieldset, Form, Span, Input, Textarea, Button, Image, ModalContent } from "./styled";
+import { dataContact } from "./dataContact";
+import dogApp from "./dog-appointment.png";
+import { useState } from "react";
 
 const AppointmentPage = () => {
+
+    const initialState = {
+        name: '',
+        email: '',
+        phone: '',
+        msg: '',
+    };
+
+    const modalState = {
+        state: false,
+        content: '',
+    }
+
+    const [values, setValues] = useState(initialState);
+    const [modal, setModal] = useState(modalState);
+
+    const handleOnChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setValues({ ...values, [name]: value });
+    }
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        const { name, email, phone, msg } = values;
+
+        if (!name || !email || !phone || !msg) {
+            return setModal({ state: true, content: "Please provide data in all fields." })
+        }
+        setModal({ state: true, content: 'Your message has been delivered.' });
+        setValues(initialState);
+    }
+
     return (
-        <div>AppointmentPage</div>
+        <Section>
+            <Header>Contact Us</Header>
+            <Container>
+                {dataContact.map((item) => {
+                    return <Tile key={item.id}>
+                        <Title>
+                            {item.title}
+                        </Title>
+                        <Paragraph>
+                            {item.subtitle}
+                        </Paragraph>
+                    </Tile>
+                })}
+            </Container>
+            <Image src={dogApp} alt="dog" />
+            <Header>Get an Appointment</Header>
+            <Form onSubmit={onFormSubmit}>
+                <Fieldset>
+                    <p>
+                        <label><Span>Full Name:</Span>
+                            <Input type="text" name="name" value={values.name} onChange={handleOnChange} />
+                        </label>
+                    </p>
+                    <p>
+                        <label><Span>E-mail address:</Span>
+                            <Input type="email" name="email" value={values.email} onChange={handleOnChange} />
+                        </label>
+                    </p>
+                    <p>
+                        <label><Span>Phone number:</Span>
+                            <Input type="tel" name="phone" value={values.phone} onChange={handleOnChange} />
+                        </label>
+                    </p>
+                    <p>
+                        <label><Span>Message:</Span>
+                            <Textarea name="msg" value={values.msg} onChange={handleOnChange} />
+                        </label>
+                    </p>
+                    <Button type="submit">Send Form</Button>
+                </Fieldset>
+            </Form>
+            {modal.state &&
+                <ModalContent>{modal.content}</ModalContent>}
+        </Section>
     )
 };
 
