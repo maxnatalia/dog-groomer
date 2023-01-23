5e9e31ebfab93edba207f981852972ffa726c72d
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-23 13:35:27 +0100

    added general style for Tile

---
+++ src/common/Tile/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Tile = styled.div`
+    padding: 10px;
+    width: 280px;
+    border-radius: 5px;
+    border: 2px solid orange;
+    box-shadow: 4px 6px 5px gray;
+    text-align: center;
+`;
