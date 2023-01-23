0630254695c133c53492d4f43bda4049d59972eb
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-23 13:35:55 +0100

    added general styles for Paragraph

---
+++ src/common/Paragraph/styled.js
@@ -0,0 +1,14 @@
+import styled, { css } from "styled-components";
+
+export const Paragraph = styled.p`
+    font-size: 22px;
+
+    @media (max-width: 767px) {
+        font-size: 18px;
+    }
+
+    ${({ hero }) => hero && css`
+        font-size: 32px;
+        font-weight: 600;
+    `}
+`;
