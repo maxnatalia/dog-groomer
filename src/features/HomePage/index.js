85b47e6d543619f0eb5c20ef7d0dee299cfa57e2
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-24 09:00:45 +0100

    added Newsletter to Homepage

--- src/features/HomePage/index.js
+++ src/features/HomePage/index.js
@@ -2,10 +2,12 @@ import Carousel from "./Carousel";
 import Content from "./Content";
 import AboutUs from "./AboutUs";
 import Testimonials from "./Testimonials";
+import Newsletter from "../../common/Newsletter";
 
 const HomePage = () => {
     return (
         <>
+            <Newsletter />
             <Carousel />
             <Content />
             <AboutUs />
