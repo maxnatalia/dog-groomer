cbdd10fd238eb839514d987367d854b0659ea9f6
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-26 08:03:12 +0100

    added box-shadows and changed hover in Buttons

--- src/common/Button/styled.js
+++ src/common/Button/styled.js
@@ -10,8 +10,12 @@ export const Button = styled(Link)`
     border: none;
     padding: 10px 20px;
     cursor: pointer;
+    box-shadow: 1px 2px 2px gray;
+    transition: 1s ease-in-out;
 
     &:hover {
         filter: brightness(120%);
+        box-shadow: 1px 5px 3px gray;
+        color: #000958;
     }
 `;
