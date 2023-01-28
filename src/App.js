fdeac309d4310157d84b5f557d972b77be79235b
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-28 08:37:02 +0100

    improved refreshe error page added HashRouter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import HomePage from "./features/HomePage";
 import PricingPage from "./features/PricingPage";
 import AppointmentPage from "./features/AppointmentPage";
 import Navigation from "./common/Navigation";
-import Reminder from "./features/Reminder";
 import Questions from "./features/Questions";
 import Staff from "./features/Staff";
 import Footer from "./common/Footer";
+import ErrorPage from "./features/ErrorPage";
+import ScrollToTop from "./common/ScrollToTop";
+import Newsletter from "./common/Newsletter";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navigation />
-      <Routes>
-        <Route path="/appointment" element={<AppointmentPage />} />
-        <Route path="/price" element={<PricingPage />} />
-        <Route path="/reminder" element={<Reminder />} />
-        <Route path="/questions" element={<Questions />} />
-        <Route path="/staff" element={<Staff />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Newsletter />
+      <ScrollToTop>
+        <Routes>
+          <Route path="/appointment" element={<AppointmentPage />} />
+          <Route path="/price" element={<PricingPage />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/staff" element={<Staff />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </ScrollToTop>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
