112e0c49170fde6d711d24344198c231eb9ba58e
maxnatalia <Nataliamazur1988@gmail.com>
2023-01-28 08:33:30 +0100

    removed Newsletter from HomePage

--- src/features/HomePage/index.js
+++ src/features/HomePage/index.js
@@ -2,12 +2,10 @@ import Carousel from "./Carousel";
 import Content from "./Content";
 import AboutUs from "./AboutUs";
 import Testimonials from "./Testimonials";
-import Newsletter from "../../common/Newsletter";
 
 const HomePage = () => {
     return (
         <>
-            <Newsletter />
             <Carousel />
             <Content />
             <AboutUs />
