ff745bcb388e8a8383277ff98843049fbd616529
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-05 11:24:29 +0100

    added new content to AboutUs

--- src/features/HomePage/AboutUs/index.js
+++ src/features/HomePage/AboutUs/index.js
@@ -1,5 +1,6 @@
 import { Header } from "../../../common/Header/styled";
 import { Title } from "../../../common/Title/styled";
+import { Paragraph } from "../../../common/Paragraph/styled";
 import { List, Wrapper, Image, Container, Item } from "./styled";
 import dogImg from "./dog3.jpg";
 
@@ -8,6 +9,35 @@ const AboutUs = () => {
         <section>
             <Header>About Us</Header>
             <Container>
+                <Title>The "Groom Dog Philosophy"</Title>
+                <Paragraph special>
+                    The "Groom Dog Philosophy" is a set of beliefs and principles that guide the approach to dog grooming. This philosophy may include the following elements:
+                </Paragraph>
+                <List>
+                    <Item>🐶Health and comfort:</Item>
+                    <Paragraph>
+                        The priority of the groom dog philosophy is to prioritize the health and comfort of the dog. This includes using gentle and safe grooming techniques, avoiding excessive handling or stress, and ensuring that the dog is relaxed throughout the grooming session.
+                    </Paragraph>
+
+                    <Item>🐶Holistic approach:</Item>
+                    <Paragraph>
+                        The groom dog philosophy recognizes that grooming is not just about making a dog look good, but it is also about their overall well-being. This may include considering the dog's coat, skin, and nail health, as well as their overall health and comfort.
+                    </Paragraph>
+
+                    <Item>🐶Customized grooming:</Item>
+                    <Paragraph>
+                        The groom dog philosophy emphasizes the importance of customized grooming to meet the individual needs of each dog. This may include considering the dog's breed, coat type, and lifestyle, as well as their specific grooming needs.
+                    </Paragraph>
+
+                    <Item>🐶Respect for the dog:</Item>
+                    <Paragraph>
+                        The groom dog philosophy is based on the belief that dogs deserve respect and humane treatment. This includes using positive reinforcement techniques, avoiding harsh or abusive handling, and treating each dog as an individual with their own unique needs and personality.
+                    </Paragraph>
+
+                    <Paragraph special>
+                        Overall, the groom dog philosophy is about providing a high-quality grooming experience that is safe, gentle, and tailored to the individual needs of each dog.
+                    </Paragraph>
+                </List>
                 <Wrapper>
                     <Title>Why Choose Us?</Title>
                     <Image src={dogImg} alt="dog img" />
