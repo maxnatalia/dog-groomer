ac2ff7b80e895aa2e3ec87d40681daf5ed8362c1
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-05 11:25:52 +0100

    improved Content component

--- src/features/HomePage/Content/contentData.js
+++ src/features/HomePage/Content/contentData.js
@@ -5,21 +5,18 @@ import dog3 from "./dog-categ-3.png";
 export const contentData = [
     {
         title: "How much does it cost?",
-        subtitle: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.",
         img: dog1,
         buttonText: "See More",
         buttonTO: "/price",
     },
     {
         title: "How Can I get appointment?",
-        subtitle: "Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus",
         img: dog2,
         buttonText: "Get Appointment",
         buttonTO: "/appointment",
     },
     {
         title: "Which services we offer?",
-        subtitle: "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod.",
         img: dog3,
         buttonText: "See More",
         buttonTO: "/questions",
--- src/features/HomePage/Content/styled.js
+++ src/features/HomePage/Content/styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Section = styled.section`
-    padding: 40px;
+    padding: 20px;
     display: flex;
     flex-direction: column;
     justify-content: center;
