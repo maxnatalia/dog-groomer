af04089a73021ce82e3fc16ea016e83b7931c53d
maxnatalia <Nataliamazur1988@gmail.com>
2023-02-05 11:26:07 +0100

    updated data in Staff

--- src/features/Staff/data.js
+++ src/features/Staff/data.js
@@ -1,39 +1,31 @@
 const people = [
     {
         id: 1,
-        image:
-            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
         name: 'maria ferguson',
-        title: 'office manager',
-        quote:
-            'Fingerstache Nulla tempor finibus nulla sit amet bibendum. Suspendisse porttitor gravida porta. Mauris ac urna consectetur, gravida risus ut, bibendum nulla. Fusce suscipit orci nec lacus consequat, eu rhoncus nulla placerat.',
+        title: 'head stylist',
+        quote: "Maria is our head stylist and is responsible for coordinating the work of the entire team. She has over 10 years of experience in the industry and is exceptionally skilled in styling and grooming all dog breeds. Anna is also a lecturer at a renowned pet grooming school, where she shares her experience and knowledge with younger stylists.",
     },
     {
         id: 2,
-        image:
-            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
         name: 'john doe',
-        title: 'senior groomer',
-        quote:
-            'Nulla tempor finibus nulla sit amet bibendum. Suspendisse porttitor gravida porta. Mauris ac urna consectetur, gravida risus ut, bibendum nulla. Fusce suscipit orci nec lacus consequat, eu rhoncus nulla placerat.',
+        title: 'senior stylist',
+        quote: "Jan is our senior stylist and has over 8 years of experience in the industry. He is a specialist in grooming and styling puppies and senior dogs, and his work is always meticulous and precise. Jan is beloved by our clients for his patience and nurturing towards all the animals that come into his hands.",
     },
     {
         id: 3,
-        image:
-            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
         name: 'peter smith',
-        title: 'groomer',
-        quote:
-            'Nulla tempor finibus nulla sit amet bibendum. Suspendisse porttitor gravida porta. Mauris ac urna consectetur, gravida risus ut, bibendum nulla. Fusce suscipit orci nec lacus consequat, eu rhoncus nulla placerat.',
+        title: 'stylist assistant',
+        quote: "Peter is our experienced stylist assistant and helps our stylists with their daily tasks. Bartłomiej is always ready to help and is known for being always smiling and ready to act. He is also learning new techniques and tools to be able to independently perform grooming services in the future.",
     },
     {
         id: 4,
-        image:
-            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
         name: 'susan andersen',
-        title: 'the boss',
-        quote:
-            'Marfa Nulla tempor finibus nulla sit amet bibendum. Suspendisse porttitor gravida porta. Mauris ac urna consectetur, gravida risus ut, bibendum nulla. Fusce suscipit orci nec lacus consequat, eu rhoncus nulla placerat. ',
+        title: 'stylist',
+        quote: "Susan is our talented stylist and has over 5 years of experience in the industry. She is a specialist in styling purebred dogs and is always ready to unleash her creativity in styling our four-legged clients. Maria is known for always putting her clients first and doing everything to provide them with the best experience.",
     },
 ];
 
