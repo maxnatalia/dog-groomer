3bd0ad98f44cbd4ccd21d3c2c421b99388bcea8f
maxnatalia <Nataliamazur1988@gmail.com>
2023-06-20 08:40:00 +0200

    added styles to StylesMapContainer

---
+++ src/features/AppointmentPage/Map/styled.js
@@ -0,0 +1,7 @@
+import { MapContainer } from "react-leaflet";
+import styled from "styled-components";
+
+export const StyledMapContainer = styled(MapContainer)`
+  height: 400px;
+  width: 100%;
+`;
