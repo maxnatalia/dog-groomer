8e298c96c93b642b643841944c8e73664978b880
maxnatalia <Nataliamazur1988@gmail.com>
2023-06-21 13:45:14 +0200

    added dataReviews for Reviews component

---
+++ src/features/pages/Home/Reviews/dataReviews.js
@@ -0,0 +1,33 @@
+export const dataReviews = [
+    {
+        id: 1,
+        name: "Becca Rodriguez",
+        info: "Incredibly professional approach to our dog! The groomer was very cautious and attentive, and our dog returned home looking like new!",
+    },
+
+    {
+        id: 2,
+        name: "Julia Roberts",
+        info: "We didn't know that a visit to the groomer could be so enjoyable for our dog! It's clear that the groomer loves animals and is really good at it.",
+    },
+    {
+        id: 3,
+        name: "Carlos Santi",
+        info: "Our dog felt like at home during the visit to the groomer! We are very satisfied with the quality of services and the groomer's commitment.",
+    },
+    {
+        id: 4,
+        name: "Sarah Jonhson",
+        info: "The visit to the groomer was a real pleasure for our dog! The groomer was so gentle and caring that our dog didn't want to leave!",
+    },
+    {
+        id: 5,
+        name: "Shanone Luz",
+        info: "We are very satisfied with the quality of services at the dog groomer! The groomer listened carefully to our requirements and achieved the perfect result for our dog.",
+    },
+    {
+        id: 6,
+        name: "Jon Jonhson",
+        info: "Our experience with the dog groomer was amazing! The groomer was so attentive and precise, and our dog looked like a million dollars after the visit. We are very satisfied and will definitely return!",
+    },
+]
