28b817b7aa0f2e733217b78cdcbea9e548795d66
maxnatalia <Nataliamazur1988@gmail.com>
2023-06-21 13:17:48 +0200

    added theme - colors and shadows

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+    colors: {
+        primary: "#e15024",
+        secondary: "#f3bf19",
+        third: "#f0dcbb",
+        light: "#fff9f2",
+        medium: "#574d44",
+        dark: "#5e1c20",
+    },
+    shadows: {
+        light: `6px 6px 13px -5px #5e1c20`,
+    },
+
+}
