df04abe8c42e38d4739d519c659df8ea11753035
maxnatalia <Nataliamazur1988@gmail.com>
2023-06-23 12:59:20 +0200

    added dataFooter

---
+++ src/features/pages/Footer/footerData.js
@@ -0,0 +1,20 @@
+import { AiFillYoutube, AiFillInstagram, AiFillTwitterCircle, AiFillFacebook } from 'react-icons/ai';
+
+export const social = [
+  {
+    icon: <AiFillYoutube />,
+    href: '#',
+  },
+  {
+    icon: <AiFillInstagram />,
+    href: '#',
+  },
+  {
+    icon: <AiFillTwitterCircle />,
+    href: '#',
+  },
+  {
+    icon: <AiFillFacebook />,
+    href: '#',
+  },
+];
