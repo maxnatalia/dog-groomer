d55b4881a8f14ca259d7c787979571fc71ea9a36
maxnatalia <Nataliamazur1988@gmail.com>
2023-06-26 11:58:49 +0200

    created contentData for Content component

---
+++ src/features/pages/Home/Content/contentData.js
@@ -0,0 +1,21 @@
+import { FaAward } from "react-icons/fa";
+import { ImHappy2 } from "react-icons/im";
+import { FaCertificate } from "react-icons/fa";
+
+export const contentData = [
+    {
+        title: "Awards",
+        img: <FaAward />,
+        number: 68,
+    },
+    {
+        title: "Happy Clients",
+        img: <ImHappy2 />,
+        number: 547,
+    },
+    {
+        title: "Experience",
+        img: <FaCertificate />,
+        number: 23,
+    },
+];
