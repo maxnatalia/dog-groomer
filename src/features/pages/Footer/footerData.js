ddc4af346f363429e78d1190cb565eab4eb5cb8b
maxnatalia <Nataliamazur1988@gmail.com>
2023-06-27 12:22:03 +0200

    updated footerData - added titles

--- src/features/pages/Footer/footerData.js
+++ src/features/pages/Footer/footerData.js
@@ -4,17 +4,21 @@ export const social = [
   {
     icon: <AiFillYoutube />,
     href: '#',
+    title: 'YouTube',
   },
   {
     icon: <AiFillInstagram />,
     href: '#',
+    title: 'Instagram',
   },
   {
     icon: <AiFillTwitterCircle />,
     href: '#',
+    title: 'Twitter',
   },
   {
     icon: <AiFillFacebook />,
     href: '#',
+    title: 'Facebook',
   },
 ];
