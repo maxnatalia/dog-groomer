a515fcc68d4abc20c19c86ba477f1c50a209fca6
maxnatalia <Nataliamazur1988@gmail.com>
2023-06-28 11:31:26 +0200

    added Navigation component to AppLayout

---
+++ src/common/AppLayout/Navigation/index.js
@@ -0,0 +1,19 @@
+import { navigationData } from './navigationData';
+import { Link, List, StyledNavigation } from './styled';
+import Logo from "../../Logo";
+
+const Navigation = () => {
+    return (
+        <StyledNavigation>
+            <Logo />
+            <List>
+                {navigationData.map((link) => <li key={link.text}>
+                    <Link to={link.to}>{link.text}</Link>
+                </li>
+                )}
+            </List>
+        </StyledNavigation>
+    )
+};
+
+export default Navigation;
---
+++ src/common/AppLayout/Navigation/navigationData.js
@@ -0,0 +1,22 @@
+export const navigationData = [
+	{
+		to: '/',
+		text: 'Home',
+	},
+	{
+		to: "/price",
+		text: 'Pricing',
+	},
+	{
+		to: "/appointment",
+		text: 'Get Appointment',
+	},
+	{
+		to: "/questions",
+		text: 'Q&A',
+	},
+	{
+		to: "/about",
+		text: 'About',
+	},
+];
