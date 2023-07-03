33c2926ff55e3c66d4640fe423d1c18c675e69aa
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-03 10:01:17 +0200

    created common Paragraph component and styles

---
+++ src/common/Paragraph/index.js
@@ -0,0 +1,9 @@
+import { StyledParagraph } from "./styled";
+
+const Paragraph = ({ children }) => {
+    return (
+        <StyledParagraph>{children}</StyledParagraph>
+    )
+};
+
+export default Paragraph;
---
+++ src/common/Paragraph/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledParagraph = styled.p`
+    padding: 10px;
+    font-size: 20px;
+    text-align: center;
+
+    @media (max-width: 767px) {
+        font-size: 16px;
+    }
+`;
