0512ac1264ad264ee02cd633f80d223dea07f594
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-03 09:59:32 +0200

    added buttonsHome for Header props in Home

---
+++ src/features/pages/Home/buttonsHome.js
@@ -0,0 +1,11 @@
+import {
+    MdPhotoLibrary,
+    MdCleaningServices,
+    MdOutlineReviews,
+} from "react-icons/md";
+
+export const buttonsHome = [
+    { href: "#services", icon: <MdCleaningServices />, text: "services", as: "a" },
+    { href: "#reviews", icon: <MdOutlineReviews />, text: "reviews", as: "a" },
+    { href: "#gallery", icon: <MdPhotoLibrary />, text: "gallery", as: "a" },
+];
