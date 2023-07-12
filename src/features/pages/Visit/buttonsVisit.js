3baa78e2a5186565048f6a60b70ad6792effef5f
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-12 12:09:07 +0200

    added buttonsVisit.js

---
+++ src/features/pages/Visit/buttonsVisit.js
@@ -0,0 +1,7 @@
+import {
+    MdCleaningServices,
+} from "react-icons/md";
+
+export const buttonsVisit = [
+    { href: "#standard", icon: <MdCleaningServices />, text: "make an appointment", as: "a" }
+];
