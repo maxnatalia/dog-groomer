2f7c0867f9b56dc40992038ac76b790fd06c61e4
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-12 12:10:16 +0200

    added buttonsOffer.js

---
+++ src/features/pages/Offer/buttonsOffer.js
@@ -0,0 +1,11 @@
+import {
+    MdPhotoLibrary,
+    MdCleaningServices,
+    MdOutlineReviews,
+} from "react-icons/md";
+
+export const buttonsOffer = [
+    { href: "#standard", icon: <MdCleaningServices />, text: "standard offer", as: "a" },
+    { href: "#premium", icon: <MdPhotoLibrary />, text: "premium offer", as: "a" },
+    { href: "#personal", icon: <MdOutlineReviews />, text: "personal offer", as: "a" },
+];
