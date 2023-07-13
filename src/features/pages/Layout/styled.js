e83270f2b408af79d7660bdd16ca4443fa29378f
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-13 10:10:11 +0200

    added styles to Layout common parent page

---
+++ src/features/pages/Layout/styled.js
@@ -0,0 +1,7 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+export const StyledAppLayout = styled(motion.div)`
+    max-width: 1400px;
+    margin: 0 auto;
+`;
