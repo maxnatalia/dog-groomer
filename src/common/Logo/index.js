a1045ace2db483fe74cbdd1d113da812e36dce8e
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-14 08:11:47 +0200

    updated Logo - added props onlyLogo

--- src/common/Logo/index.js
+++ src/common/Logo/index.js
@@ -1,11 +1,16 @@
 import logo from "./logo.png";
 import { Company, StyledLogo, Wrapper } from './styled';
 
-const Logo = () => {
+const Logo = ({ onlyLogo }) => {
     return (
         <Wrapper to={"/"} title={"Home"}>
-            <StyledLogo src={logo} alt="logo" />
-            <Company>Dog-Groomer</Company>
+            {onlyLogo
+                ? <StyledLogo src={logo} alt="logo" />
+                : <>
+                    <StyledLogo src={logo} alt="logo" />
+                    <Company>Dog-Groomer</Company>
+                </>
+            }
         </Wrapper>
     )
 };
