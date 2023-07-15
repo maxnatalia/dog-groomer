a2d1c3bc0ab5b052ed999c1570a444062567cb57
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-15 07:17:12 +0200

    added Footer and Contact to Layout

---
+++ src/features/pages/Layout/Footer/footerData.js
@@ -0,0 +1,24 @@
+import { AiFillYoutube, AiFillInstagram, AiFillTwitterCircle, AiFillFacebook } from 'react-icons/ai';
+
+export const social = [
+  {
+    icon: <AiFillYoutube />,
+    href: '#',
+    title: 'YouTube',
+  },
+  {
+    icon: <AiFillInstagram />,
+    href: '#',
+    title: 'Instagram',
+  },
+  {
+    icon: <AiFillTwitterCircle />,
+    href: '#',
+    title: 'Twitter',
+  },
+  {
+    icon: <AiFillFacebook />,
+    href: '#',
+    title: 'Facebook',
+  },
+];
