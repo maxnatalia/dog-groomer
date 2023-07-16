96e4e0a568b56b3e3aa1a7d49dc703e63f30eca2
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-16 11:42:04 +0200

    created compound component Modal

---
+++ src/common/Modal/index.js
@@ -0,0 +1,44 @@
+import { useState, useContext, cloneElement, createContext } from "react";
+import { createPortal } from "react-dom";
+import { HiXMark } from "react-icons/hi2";
+import Button from "../Button";
+import { Overlay, StyledModal } from "./styled";
+
+const ModalContext = createContext();
+
+const Modal = ({ children }) => {
+    const [openName, setOpenName] = useState('');
+
+    const close = () => setOpenName('');
+    const open = (name) => setOpenName(name);
+
+    return (<ModalContext.Provider value={{ openName, close, open }}>{children}</ModalContext.Provider>)
+};
+
+const Open = ({ children, opens: opensWindowName }) => {
+    const { open } = useContext(ModalContext);
+
+    return (cloneElement(children, { onClick: () => open(opensWindowName) }));
+};
+
+const Window = ({ children, name }) => {
+    const { openName, close } = useContext(ModalContext);
+
+    if (name !== openName) return null;
+
+    return (createPortal(
+        <Overlay>
+            <StyledModal>
+                <Button onClick={close}>
+                    <HiXMark />
+                </Button>
+                <div>{cloneElement(children, { onCloseModal: close })}</div>
+            </StyledModal>
+        </Overlay>, document.body)
+    )
+};
+
+Modal.Open = Open;
+Modal.Window = Window;
+
+export default Modal;
