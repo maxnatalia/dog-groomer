ebb672424314ab343ad9c43886ea022b0c070be6
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-20 12:16:37 +0200

    added dataProducts for Products component

---
+++ src/features/pages/About/Products/dataProducts.js
@@ -0,0 +1,45 @@
+import image1 from "./1.jpg";
+import image2 from "./2.jpg";
+import image3 from "./3.jpg";
+import image4 from "./4.jpg";
+import image5 from "./5.jpg";
+import image6 from "./6.jpg";
+
+export const dataProducts = [
+    {
+        id: 1,
+        title: "Individual approach",
+        content: "At our salon, we understand that each dog is unique and has specific needs. Therefore, we meticulously choose grooming products that best suit your pet's individual requirements.",
+        image: image1,
+    },
+    {
+        id: 2,
+        title: "Knowledge and experience",
+        content: "Our team of groomers is highly qualified, possessing the necessary skills and experience in working with various dog breeds. This enables us to utilize top-quality products in a way that delivers the best results, leaving your dog happy and content.",
+        image: image2,
+    },
+    {
+        id: 3,
+        title: "Safety of your pet",
+        content: "We only use products specifically designed for the care and grooming of dogs. Our products are meticulously chosen to ensure the highest level of safety for your dog.Ingredients used in our products are carefully checked for potential allergens and toxic substances.",
+        image: image3,
+    },
+    {
+        id: 4,
+        title: "Professional brands",
+        content: "We exclusively collaborate with reputable manufacturers and brands known for their reliability and quality. Our products are recommended by professional groomers and veterinarians.",
+        image: image4,
+    },
+    {
+        id: 5,
+        title: "Effectiveness",
+        content: "Our grooming products provide exceptional effectiveness. Brushes, shampoos, conditioners, and other grooming accessories are carefully selected to guarantee the best results in maintaining your dog's healthy and beautiful appearance.",
+        image: image5,
+    },
+    {
+        id: 6,
+        title: "Environmental care",
+        content: "We are also conscious of the impact on the natural environment. That's why most of our products are environmentally friendly, biodegradable, or fully renewable.",
+        image: image6,
+    },
+]
