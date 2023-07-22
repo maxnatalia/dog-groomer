7b6cbdafd020a2b204e683ba911a7496cf6f4b66
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-22 10:27:26 +0200

    added styles to Resume component

---
+++ src/common/Visit/VisitForm/Resume/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    margin: 40px;
+    width: 600px;
+    word-break: break-word;
+
+    @media (max-width: 767px) {
+        width: 260px;
+    }
+`;
