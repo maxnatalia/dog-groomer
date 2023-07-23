73fa215d59c9dea1513aeafdd2bec5b8a2fa41e3
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-23 08:23:26 +0200

    added buttonsAbout for AboutPage

---
+++ src/features/pages/About/buttonsAbout.js
@@ -0,0 +1,11 @@
+import {
+    MdPhotoLibrary,
+    MdCleaningServices,
+    MdOutlineReviews
+} from "react-icons/md";
+
+export const buttonsAbout = [
+    { href: "#general", icon: <MdCleaningServices />, text: "about us", as: "a" },
+    { href: "#staff", icon: <MdOutlineReviews />, text: "about staff", as: "a" },
+    { href: "#products", icon: <MdPhotoLibrary />, text: "about products", as: "a" },
+];
