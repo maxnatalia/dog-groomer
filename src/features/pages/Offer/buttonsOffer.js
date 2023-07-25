7aa650361fe7591ac2b1f4b25cf029c828363bb6
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-25 11:31:02 +0200

    improved hrefs for buttons

--- src/features/pages/Offer/buttonsOffer.js
+++ src/features/pages/Offer/buttonsOffer.js
@@ -5,7 +5,7 @@ import {
 } from "react-icons/md";
 
 export const buttonsOffer = [
-    { href: "#standard", icon: <MdCleaningServices />, text: "standard offer", as: "a" },
+    { href: "#smart", icon: <MdCleaningServices />, text: "smart offer", as: "a" },
     { href: "#premium", icon: <MdPhotoLibrary />, text: "premium offer", as: "a" },
-    { href: "#personal", icon: <MdOutlineReviews />, text: "personal offer", as: "a" },
+    { href: "#royal", icon: <MdOutlineReviews />, text: "royal offer", as: "a" },
 ];
