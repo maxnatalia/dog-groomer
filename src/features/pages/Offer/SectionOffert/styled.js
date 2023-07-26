bc49d059e4d8ebf374af480e095155cce967bf2d
maxnatalia <Nataliamazur1988@gmail.com>
2023-07-26 10:36:16 +0200

    added styles to SectionOffer

---
+++ src/features/pages/Offer/SectionOffert/styled.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    padding: 20px;
+`;
+
+export const List = styled.ul`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-left: 0;
+`;
+
+export const Item = styled.li`
+    padding: 10px 16px;
+    font-weight: 600;
+`;
+
+export const Box = styled.div`
+    flex: 1;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+    gap: 10px;
+`;
+
+export const Tile = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: ${({ theme }) => theme.colors.secondary};
+    background-color: ${({ theme }) => theme.colors.dark};
+    box-shadow: ${({ theme }) => theme.shadows.light};
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+`;
+
+export const Image = styled.img`
+    width: 100px;
+    height: 100px;
+`;
