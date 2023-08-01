e4eab5696a1ec1e92acd02b2b4a1cb6b3b77e997
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-01 10:42:08 +0200

    improved imports for index

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Normalize } from 'styled-normalize';
+import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from "./GlobalStyle";
+import { theme } from "./theme";
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Normalize />
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <Normalize />
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
