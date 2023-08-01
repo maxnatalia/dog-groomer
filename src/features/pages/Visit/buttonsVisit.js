01eca8d9c5c3bb2227a1942a4ce4cb3b7bf23633
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-01 10:41:00 +0200

    deleted unnecessary files

--- src/features/pages/Visit/buttonsVisit.js
+++
@@ -1,7 +0,0 @@
-import {
-    MdCleaningServices,
-} from "react-icons/md";
-
-export const buttonsVisit = [
-    { href: "#standard", icon: <MdCleaningServices />, text: "make an appointment", as: "a" }
-];
