b3188d621b590d676a42618b2e4c3b93d7218156
maxnatalia <Nataliamazur1988@gmail.com>
2023-08-01 10:41:54 +0200

    improved paths and imports for App

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./features/pages/Home";
 import About from "./features/pages/About";
 import Offer from "./features/pages/Offer";
 import Error from "./features/pages/Error";
-import Visit from "./features/pages/Visit";
 import Layout from "./features/pages/Layout";
 
 const App = () => {
@@ -16,7 +15,6 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/offer" element={<Offer />} />
-            <Route path="/visit" element={<Visit />} />
           </Route>
           <Route path="*" element={<Error />} />
         </Routes>
